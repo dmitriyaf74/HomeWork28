0fef8f9e4be81f2b98f65ac47f0da138ace1426a
Dmitriy Filippov <dmitriy.filippov.reid@gmail.com>
2025-03-09 10:03:20 +0500

    Добавьте файлы проекта.

---
+++ Classes/OtusDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HomeWork28.Classes
+{
+    internal class OtusDictionary
+    {
+        private int capacity;
+        private int[] array;
+
+
+        //array  capacity=4
+        //Capacity (get; set;)
+        //set
+
+        public OtusDictionary()
+        {
+            capacity = 4;
+            array = new int[capacity];
+        }
+
+
+        public int Capacity {
+            get 
+            {
+                return capacity;
+            } 
+            set
+            { 
+                capacity = value; 
+            }
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using HomeWork28.Classes;
+
+namespace HomeWork28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var otusDictionary = new OtusDictionary();
+            Console.WriteLine(otusDictionary.Capacity);
+        }
+    }
+}
