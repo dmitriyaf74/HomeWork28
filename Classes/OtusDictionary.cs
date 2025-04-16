af9dd9f66f5569b7eb139e6b33ab78379d600268
Dmitriy Filippov <dmitriy.filippov.reid@gmail.com>
2025-04-16 10:09:17 +0500

    fix: добавлен HASH поиск и индексатор

--- Classes/OtusDictionary.cs
+++ Classes/OtusDictionary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -7,104 +8,110 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using HomeWork28.Exceptions;
 using HomeWork28.Interfaces;
+using Microsoft.VisualBasic;
 using static System.Math;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace HomeWork28.Classes
 {
-    internal class OtusDictionary: IMyList
+    internal class OtusDictionary : IMyList
     {
-        private int _capacity;
         private int _size;
         private DictElement[] _array;
-        public int Capacity {  get { return _capacity; } }
+        public int Capacity { get { return _array.Length; } }
 
         public OtusDictionary()
         {
             _size = 0;
-            _capacity = 32;
-            _array = new DictElement[_capacity];
+            _array = new DictElement[32];
         }
 
-        public int Size
+        public int Size { get { return _size; } }
+
+        
+        private void Grow(int NewSize)
         {
-            get
+            if (NewSize > _array.Length)
             {
-                return _size;
-            }
-            set
-            {
-                if (_size != value)
+                int newCapacity = Max(NewSize, _array.Length * 2);
+
+                DictElement[] newArray = new DictElement[newCapacity];
+                for (int i = 0; i < _array.Length; i++)
                 {
-                    if (value < 0)
+                    if (_array[i] is not null)
                     {
-                        _size = 0;
-                        return;
+                        DictElement de = _array[i];
+                        var index = de.Key;
+                        index = index % newCapacity;
+                        newArray[index] = de;
                     }
-                    else
-                    if (value > _capacity)
-                    {
-                        Grow(_size + 1);
-                    }
-                    _size = value;
-
                 }
+                _array = newArray;
             }
-
         }
 
-        private void Grow(int NewCapacity)
-        {
-            if (NewCapacity <= _capacity) 
-            {
-                return;
-            }
-            int newCapacity = Max(_capacity, 2);
-            newCapacity = Max(newCapacity, _capacity * 2);
-
-            DictElement[] newArray = new DictElement[newCapacity];
-            for (int i = 0; i < _capacity; i++)
-            {
-                if (_array[i] is not null)
-                {
-                    DictElement de = _array[i];
-                    var index = de.GetHashCode();
-                    index = index % newCapacity;
-                    newArray[index] = de;
-                }
-            }
-            _array = newArray;
-            _capacity = newCapacity;
-        }
 
         public void Add(int key, string value)
         {
-            Size = _size + 1;
+            if (_size == _array.Length)
+            {
+                Grow(_size + 1);
+            }
+
             DictElement de = new(key, value);
-            var index = de.GetHashCode();
-            index = index % _capacity;
+            var index = key % Capacity;
             while (_array[index]?.Key is not null)
             {
-                Grow(_capacity + 1);
-                index = de.GetHashCode() % _capacity;
+                Grow(_array.Length + 1);
+                index = key % Capacity;
             }
             _array[index] = de;
+            _size++;
+        }
+
+        public string HachSearchStr(int key)
+        {
+            int index = key % Capacity;
+            int startIndex = index; 
+
+            while (_array[index] != null)
+            {
+                if (_array[index].Key == key)
+                {
+                    return _array[index].Name; 
+                }
+
+                index = (index + 1) % Capacity; 
+
+                if (index == startIndex)
+                {
+                    return null; 
+                }
+            }
+
+            return null; 
         }
 
         public string Get(int key)
         {
-            for (int i = 0; i < _capacity; i++)
-            {
-                if (_array[i]?.Key == key)
-                {
-                    return _array[i].Name;
-                }
-            }
+            var resStr = HachSearchStr(key);
+            if (resStr != null)
+                return resStr;
             throw new EKeyNotFound($"Ключ не найден {key}");
         }
 
         public DictElement IndexOf(int index)
         {
             return _array[index];
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                return Get(index);
+            }
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,10 +9,13 @@ namespace HomeWork28
             var otusDictionary = new OtusDictionary();
             otusDictionary.Add(1, "qwe");
             otusDictionary.Add(2, "sdasd");
-            otusDictionary.Add(3, "3qwe");
-            otusDictionary.Add(4, "41sdasd");
-            otusDictionary.Add(4, "42sdasdsax");
+            otusDictionary.Add(4, "sdasd444");
+            otusDictionary.Add(36, "36 sdasd444");
+            otusDictionary.Add(598, "3qwe");
+            otusDictionary.Add(235, "41sdasd");
+            otusDictionary.Add(1444, "1444 sdasdsax");
 
+            Console.WriteLine("Перебор массива:");
             for (int i = 0; i < otusDictionary.Capacity; i++)
             {
                 var de = otusDictionary.IndexOf(i);
@@ -21,10 +24,19 @@ namespace HomeWork28
                     Console.WriteLine($"{otusDictionary.IndexOf(i).Key}={otusDictionary.IndexOf(i).Name}");
                 }
             }
+
+            Console.WriteLine("");
+            Console.WriteLine("Параметры массива:");
+
             Console.WriteLine($"Capacity={otusDictionary.Capacity}");
             Console.WriteLine($"Size={otusDictionary.Size}");
-            Console.WriteLine($"3={otusDictionary.Get(3)}");
-            Console.WriteLine(otusDictionary.Get(12));
+
+            Console.WriteLine("");
+            Console.WriteLine("Удачный поиск:");
+            Console.WriteLine($"1444={otusDictionary[1444]}");
+            Console.WriteLine("");
+            Console.WriteLine("Неудачный поиск (12):");
+            Console.WriteLine($"12={otusDictionary[12]}");
         }
     }
 }
